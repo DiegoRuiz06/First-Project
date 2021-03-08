3a9a177214139685e2e97f51cbf3f93dd84f884a
diegoruiz06 <diegoruiz0196@gmail.com>
2021-03-08 17:22:06 -0600

    finished html and starting functions js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
     /*----- constants -----*/
     const wordBank = ["nebula", "comet", "planet", "star", "asteroid", "meteor", "galaxy", "gravity", "universe", "moon", "constellation"]
-    const guessesRemaning = 10;
+    const guessesRemaning = [10]
 
 
 
@@ -11,9 +11,9 @@
     let blanks = 0;
     let blanksAndCorrect = [];
     // counter variables
-    var wins = 0;
-    var losses = 0;
-    var remainingGuesses
+    let wins = 0;
+    let losses = 0;
+    let remainingGuesses
 
     /*----- cached element references -----*/
     const scoreEls = {
@@ -24,13 +24,55 @@
     const guessedEls = {
         wrong: document.querySelector("#playerguesses"),
     }
-    console.log(guessedEls);
+    // console.log(guessedEls);
 
     /*----- event listeners -----*/
+    document.onkeyup = function (evt) {
 
+    }
 
-
+   
 
 
     /*----- functions -----*/
+function init() {
+    //generate random word from words array//
+    randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    //split the individual word into seperate arrays, and put in new array//
+    lettersOfWord = randomWord.split("");
+    //use something to generate "_" for each letter in the array stored in the blanks (maybe a loop?)
+
+    //store length of word in blanks, for later use
+    blanks = lettersOfWord.length; 
+    
+    for (let i = 0; i < blanks; i++) {
+        blanksAndCorrect.push("_");
+    }
+
+    //show the "_" within HTML
+    document.getElementById("currentword").innerHTML = " " + blanksAndCorrect.join(" ");
+    //console logs:
+    console.log(randomWord);
+    console.log(lettersOfWord);
+    console.log(blanks);
+    console.log(blanksAndCorrect);
+}
+
+function reset() {
+    guessesRemaning = 10;
+    wrongGuess = [];
+    blanksAndCorrect = [];
+    init()
+}
+
+//If else to see if letter selected matches the random word
+function checkLetters(letter) {
+    let letterInWord = false;
+    //if the generated randomword is equal to the letter entered then the variable is true
+    for (let i = 0; i < blanks; i++) {
+        if (randomWord[i] === letter) {
+            letterInWord = true;
+        }
+    }
+}
 
